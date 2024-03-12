19543b861eec5762638d4422ffe492b176d6c7bd
Stella Redler <stella.redler@elev.ga.ntig.se>
2024-03-12 11:21:28 +0100

    update 14

---
+++ Sjödala/js/main.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    let menu = document.querySelector(".menu")
+    menu.classList.toggle("show");
+
+    let layer = document.querySelector(".fade-layer")
+    layer.classList.toggle("visible")
+}

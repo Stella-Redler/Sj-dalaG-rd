20b3d479928448336faaafe7a6348fce2a48cc38
Stella Redler <stella.redler@elev.ga.ntig.se>
2024-04-24 13:16:04 +0200

    update 22

--- Sjödala/js/main.js
+++ Sjödala/js/main.js
@@ -8,19 +8,17 @@ function showMenu() {
     layer.classList.toggle("visible")
 }
 
-/*for(let i = 0; i < img_vara_hastar.length; i+=1){
-    var img = document.getElementById('myImg');
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-    }
+var modal = document.getElementById("myModal");
 
-    var span = document.getElementsByClassName("close")[0];
+var img = document.getElementsByClassName(".img_vara_hastar");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
-    span.onclick = function() { 
-        modal.style.display = "none";
-    }
-}*/
+span.onclick = function() { 
+    modal.style.display = "none";
+}
